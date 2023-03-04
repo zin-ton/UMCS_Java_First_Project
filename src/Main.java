1ed0e9c26c0c27956094470ca7f8d2d051a5da57
zinton <anton.zhylitski@gmail.com>
2023-03-04 02:04:33 +0100

    idk if i have done this shit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(5,3);
-        Point point1 = new Point(1,1);
+        Point point = new Point(1,1);
+        Point point1 = new Point(3,1);
         Segment segment = new Segment();
         segment.setPoints(point, point1);
-        segment.length();
-        System.out.println(segment.toSvg());
+        //segment.length();
+        //System.out.println(segment.toSvg());
+        Point first = new Point(2,2);
+        Segment answer = segment.paral(segment, first);
+        answer.print_points();
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -12,8 +12,14 @@ public class Segment {
         this.point1 = point1;
         this.point2 = point2;
     }
-    public void length(){
-        System.out.println(Math.sqrt(Math.pow(point1.x-point2.x,2)+Math.pow(point1.y-point2.y,2)));
+    public double length(){
+        return Math.sqrt(Math.pow(point1.x-point2.x,2)+Math.pow(point1.y-point2.y,2));
+    }
+    public Point returnPoint1(){
+        return this.point1;
+    }
+    public Point returnPoint2(){
+        return this.point2;
     }
     public void print_points(){
         System.out.println(point1.x);
@@ -49,5 +55,58 @@ public class Segment {
         ans += '"';
         ans+="/>";
         return ans;
+    }
+    public Segment paral(Segment segment, Point point){
+        double size =  segment.length();
+        Segment answer = new Segment();
+        Point pointAns;
+        double lx=0;
+        double ly=0;
+        Point point1 = segment.returnPoint1();
+        Point point2 = segment.returnPoint2();
+        int isUper = 0;
+        double deltaY = point1.y - point2.y;
+        double deltaX = point1.x - point2.x;
+        deltaY/=deltaX;
+        deltaX = point.x - point1.x;
+        if(point.y >= (deltaY*deltaX)) isUper = 1;
+        if((point1.y <= point2.y) && isUper == 1){
+            deltaY = point1.y - point2.y;
+            lx = point.x + deltaY;
+            ly = (Math.sqrt(size*size-(point1.x-lx)*(point1.x-lx))-point.y)*-1;
+            pointAns = new Point(lx, ly);
+            answer.setPoints(point, pointAns);
+            return answer;
+        }
+        if((point1.y <= point2.y) && isUper == 0){
+            deltaY = point1.y - point2.y;
+            lx = point.x - deltaY;
+            ly = (Math.sqrt(size*size-(point1.x-lx)*(point1.x-lx))-point.y)*-1;
+            pointAns = new Point(lx, ly);
+            answer.setPoints(point, pointAns);
+            return answer;
+        }
+        else{
+            if((point1.y > point2.y) && isUper == 1){
+                deltaY = point1.y - point2.y;
+                lx = point.x - deltaY;
+                ly = (Math.sqrt(size*size-(point1.x-lx)*(point1.x-lx))-point.y)*-1;
+                pointAns = new Point(lx, ly);
+                answer.setPoints(point, pointAns);
+                return answer;
+            }
+            else{
+                if((point1.y > point2.y) && isUper == 0){
+                    deltaY = point1.y - point2.y;
+                    lx = point.x + deltaY;
+                    ly = (Math.sqrt(size*size-(point1.x-lx)*(point1.x-lx))-point.y)*-1;
+                    pointAns = new Point(lx, ly);
+                    answer.setPoints(point, pointAns);
+                    return answer;
+                }
+                else return answer;
+            }
+        }
+
     }
 }
